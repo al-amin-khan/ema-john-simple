df2087dbf8120a0c1e3c2877fe329ea427611eb1
al-amin-khan <alaminkhan2010@gmail.com>
2020-08-27 00:11:10 +0600

    shop added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Shop.css'
 import fakeData from '../../fakeData';
+import {useState} from 'react'
 
-const Shop = ({}) => (
-  <div>
-    <h1>This is Shop</h1>
-  </div>
-);
+
+const Shop = ({}) => {
+  const fakeFirst10Data = fakeData.slice(0, 10);
+  const[products, setProducts] = useState(fakeFirst10Data);
+  console.log(useState(fakeFirst10Data));
+  return(
+    <div className='shop-container'>
+      <div className='product-container'>
+        <ul>
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ul>
+      </div>
+      <div className= 'cart-container'>
+        <h3>This is cart</h3>
+      </div>
+    </div>
+
+  )
+};
 
 export default Shop;
